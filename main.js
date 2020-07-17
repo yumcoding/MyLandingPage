8b450300539a245210ea13559f1ea175dacee05e
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-17 15:45:05 +0200

    show and close modal

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const toggleBtn = document.getElementById("toggle-btn");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const modalContainer = document.getElementById("modal");
 
-toggleBtn.addEventListener("click", (e) => {
+// show nav bar
+toggleBtn.addEventListener("click", () => {
   document.querySelector("body").classList.toggle("shownav");
+});
+
+// open and close modal
+
+open.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  e.target.classList.contains("modal-container")
+    ? (modalContainer.style.display = "none")
+    : false;
 });
